0d7f48990de8d3fdcc18db92a0dd4d262cb1cad1
Frikhafeyez <frikhafeyez@gmail.com>
2020-04-22 12:28:10 +0100

    first commit

---
+++ Projet-Feyez-Frikha-Oracl-java/src/tn/iit/glid/m/MainFrame.java
@@ -0,0 +1,27 @@
+package tn.iit.glid.m;
+
+import javax.swing.JFrame;
+
+import tn.iit.glid.auth.LoginPanel;
+
+public class MainFrame extends JFrame {
+	
+	private static final long serialVersionUID = 1L;
+	private Panel mainPanel;
+	private LoginPanel loginPanel;
+
+	public MainFrame(LoginPanel loginPanel) {
+		this.loginPanel = loginPanel;
+		mainPanel = new Panel(this);
+		setSize(800, 600);
+		setContentPane(mainPanel);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(true);
+		setVisible(true);
+	}
+
+	public LoginPanel getLoginPanel() {
+		return loginPanel;
+	}
+		
+}
